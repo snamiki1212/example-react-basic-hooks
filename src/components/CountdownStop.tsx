b0546c4ec3881cf8e64157cd7882f1cb944e1f25
snamiki1212 <snamiki1212@gmail.com>
2019-05-06 18:47:27 +0700

    Add stop feature

---
+++ src/components/CountdownStop.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CountdownStop = ({onClick}: any) => {
+  return <button onClick={onClick}>STOP</button>;
+};
+
+export default CountdownStop;
--- src/containers/Countdown.tsx
+++ src/containers/Countdown.tsx
@@ -1,13 +1,17 @@
 import React, {FC, useEffect, useState} from 'react';
 import CountdownComponent from '../components/Countdown';
+import CountdownStop from '../components/CountdownStop';
 
 const useCountdown = (limit: number): [number, any] => {
   const [left, setLeft] = useState(limit);
+  const [timerObj, setTimerObj]: any = useState('');
 
   const setCountdown = () =>
-    setInterval(() => {
-      setLeft(prev => prev - 1);
-    }, 1000);
+    setTimerObj(() =>
+      setInterval(() => {
+        setLeft(prev => prev - 1);
+      }, 1000)
+    );
 
   const afterTimeup = (left: any): any => {
     if (left <= 0) {
@@ -26,13 +30,27 @@ const useCountdown = (limit: number): [number, any] => {
     afterTimeup(left);
   }, [left]);
 
-  return [left, resetLeft];
+  const handleStop = () => clearInterval(timerObj);
+
+  const restart = () => {
+    clearInterval(timerObj);
+    setCountdown();
+  };
+
+  return [left, [resetLeft, handleStop, restart]];
 };
 
 const CountdownContainer: FC = () => {
   const TIMER = 4;
-  const [left, resetLeft] = useCountdown(TIMER);
-  return <CountdownComponent left={left} resetLeft={resetLeft} />;
+  const [left, [resetLeft, handleStop, restart]] = useCountdown(TIMER);
+
+  return (
+    <>
+      <CountdownComponent left={left} resetLeft={resetLeft} />
+      <button onClick={restart}>START</button>
+      <CountdownStop onClick={handleStop} />
+    </>
+  );
 };
 
 export default CountdownContainer;
