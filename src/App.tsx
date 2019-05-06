22cb303df6c23e707a495f36c19386e6eaa12d7a
snamiki1212 <snamiki1212@gmail.com>
2019-05-06 17:12:12 +0700

    Add countdown feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App: React.FC = () => {
+  const TIMER = 5;
+  const [left, setLeft] = useState(TIMER);
+
+  const setCountdown = () => setInterval(() => setLeft(prev => prev - 1), 1000);
+  const afterTimeup = (left: number) => {
+    if (left <= 0) {
+      alert('FINISHED');
+      setLeft(prev => TIMER);
+    }
+  };
+
+  useEffect(() => {
+    setCountdown();
+  }, []);
+
   return (
     <div className="App">
+      {left}
+      {afterTimeup(left)}
     </div>
   );
-}
+};
 
 export default App;
