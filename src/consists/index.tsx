94656219810dd8d42c3e2482683f38bd3f03bbe8
snamiki1212 <snamiki1212@gmail.com>
2019-05-06 23:14:02 +0700

    Update repo url

--- src/consists/index.tsx
+++ src/consists/index.tsx
@@ -1,3 +1,4 @@
 export const APP_NAME: string = 'POMOTIMER with HOOKS';
-export const REPOSITORY_URL: string = '';
+export const REPOSITORY_URL: string =
+  'https://github.com/snamiki1212/pomotimer-with-hooks';
 export const PRIMARY_COLOR: string = 'lightgray';
