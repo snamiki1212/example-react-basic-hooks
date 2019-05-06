72b8f660ea94b8fbca54169a6fb356a90faa8c91
snamiki1212 <snamiki1212@gmail.com>
2019-05-06 21:08:17 +0700

    Replace Button as Material ui

--- src/containers/Countdown.tsx
+++ src/containers/Countdown.tsx
@@ -1,6 +1,6 @@
 import React, {FC, useEffect, useState} from 'react';
 import CountdownComponent from '../components/Countdown';
-import CountdownButton from '../components/CountdownButton';
+import Button from '@material-ui/core/Button';
 
 const useCountdown = (limit: number): any => {
   const [left, setLeft]: [number, any] = useState(limit);
@@ -24,14 +24,18 @@ const useCountdown = (limit: number): any => {
     }
   };
 
-  const reset = () => setLeft(limit);
+  const reset = () => {
+    if (window.confirm('RESET TIMER?')) {
+      setLeft(limit);
+    }
+  };
 
   const stop = () => {
     setActive(false);
     clearInterval(timerObj);
   };
 
-  const restart = () => {
+  const start = () => {
     if (!active) {
       clearInterval(timerObj);
       setCountdown();
@@ -48,19 +52,25 @@ const useCountdown = (limit: number): any => {
     afterTimeup(left);
   }, [left]);
 
-  return [[left, active], [reset, stop, restart]];
+  return [[left, active], [reset, stop, start]];
 };
 
 const CountdownContainer: FC = () => {
   const TIMER = 4;
-  const [[left, active], [reset, stop, restart]] = useCountdown(TIMER);
+  const [[left, active], [reset, stop, start]] = useCountdown(TIMER);
 
   return (
     <>
       <CountdownComponent left={left} />
-      <CountdownButton buttonType="START" onClick={restart} active />
-      <CountdownButton buttonType="STOP" onClick={stop} active />
-      <CountdownButton buttonType="RESET" onClick={reset} />
+      <Button onClick={start} disabled={active} color="primary">
+        START
+      </Button>
+      <Button onClick={stop} disabled={!active} color="primary">
+        STOP
+      </Button>
+      <Button onClick={reset} color="secondary">
+        RESET
+      </Button>
     </>
   );
 };
