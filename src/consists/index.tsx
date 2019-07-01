4a655c5625d00ad696eaefeea9de97e162472718
snamiki1212 <snamiki1212@gmail.com>
2019-07-01 20:15:10 +0900

    Update

--- src/consists/index.tsx
+++ src/consists/index.tsx
@@ -1,4 +1,4 @@
 export const APP_NAME: string = 'POMOTIMER with HOOKS';
 export const REPOSITORY_URL: string =
-  'https://github.com/snamiki1212/pomotimer-with-hooks';
+  'https://github.com/snamiki1212/example-react-basic-hooks';
 export const PRIMARY_COLOR: string = 'lightgray';
